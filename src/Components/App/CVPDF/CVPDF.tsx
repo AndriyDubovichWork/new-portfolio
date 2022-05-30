494a7ecbde6e8729fc79e6c1099ff55cab2e6679
AndriyDubovichWork <andriydubovichwork@gmail.com>
2022-05-30 17:18:01 +0300

    on cv click opens new tab with cv

--- src/Components/App/CVPDF/CVPDF.tsx
+++ src/Components/App/CVPDF/CVPDF.tsx
@@ -3,17 +3,19 @@ import style from './CVPDF.module.scss';
 import { Document, Page, pdfjs } from 'react-pdf';
 //@ts-ignore
 import CV from './../../../assets/AndriyDubovichCV.pdf';
+import download from 'downloadjs';
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const CVPDF = () => {
   return (
     <div className={style.main}>
       <h1 className={style.Title}>My Cv</h1>
-      <div className={style.File}>
+      <a className={style.File} href={CV} target='_blank'>
         <Document file={CV} onLoadSuccess={() => {}}>
           <Page pageNumber={1} width={window.screen.width - 20} />
         </Document>
-      </div>
+      </a>
     </div>
   );
 };
